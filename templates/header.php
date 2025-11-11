6d555476d72cdbc3fc973201a19dd089cc7f8170
Vlad <onixxwork@gmail.com>
2025-11-11 12:42:34 -0500

    adds test templates
    These tamplates will be reconfigured later

---
+++ templates/header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- In the head element, we add our metadata -->
+    <!-- Declare our character set -->
+    <meta charset="utf-8">
+    <!-- This measures the viewport of the device we are using -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- This will describe our pages content, this should be different
+      on each page -->
+    <meta name="description" content="<?php echo $pageDesc; ?>">
+    <!-- This will tell the search engine to index the site or not -->
+    <meta name="robots" content="noindex, nofollow">
+    <!-- This is our title tag  -->
+    <title> <?php echo $pageTitle ?></title>
+    <!-- This is the element that connects css file to our html_css_js_introduction file -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Using a special google font -->
+</head>
