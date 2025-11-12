8845aacf00d804f298171ce343fd895aa001d26c
Vlad <onixxwork@gmail.com>
2025-11-12 14:23:01 -0500

    Design the majority of the home page
    Designed the majority of the home page, added some images, fixes some css as well

--- home.php
+++ home.php
@@ -9,37 +9,46 @@
             <h1>Bring the music back to life</h1>
             <p>Review the golden era from sound with these timeless devices</p>
         </div>
-        <div>
-            <a href="shop.php" class="button-shop">Shop Now</a>
-            <a href="about.php" class="button-about">Learn More</a>
+        <div class="buttons">
+            <a href="../shop.php" class="button-home-primary">Shop Now</a>
+            <a href="../about.php" class="button-home-secondary">Learn More</a>
         </div>
     </section>
+    <section class="info-home-header">
+        <h2>Discover our classics</h2>
+    </section>
     <section class="info-home-page">
-        <div class="info-home-header">
-            <h2>Discover our classics</h2>
+        <!-- Big iPod with headphones -->
+        <div class="ipod-headphones">
+            <img src="../images/ipod_headphones_home_page.png" alt="iPod with headphones">
         </div>
-        <div class="ipod-cards">
-            <div class="ipod-type">
-                <img src="./images/ipod_headphones_home_page.png" alt="ipod headphones">
-            </div>
-            <div class="ipod-type">
-                <img src="./images/ipod_classic_home_page.png" alt="ipod classic">
-                <p>Ipod Classic</p>
-            </div>
-            <div class="ipod-type">
-                <img src="./images/ipod_mini_home_page.png" alt="ipod mini">
-                <p>Ipod Mini</p>
-            </div>
-            <div class="info-home-ipod-shuffle">
-                <img src="./images/ipod_shuffle_home_page.png" alt="ipod shuffle">
-                <p>Ipod Shuffle</p>
-            </div>
+        <!-- Three small iPods which will used as small image cards -->
+        <div class="ipod-card">
+            <img src="../images/ipod_classic_home_page.png" alt="iPod Classic">
+            <h4>iPod Classic</h4>
         </div>
-        <div class="info-home-add-info">
+        <div class="ipod-card">
+            <img src="../images/ipod_mini_home_page.png" alt="iPod Mini">
+            <h4>iPod Mini</h4>
+        </div>
+        <div class="ipod-card">
+            <img src="../images/ipod_shuffle_home_page.png" alt="iPod Shuffle">
+            <h4>iPod Shuffle</h4>
+        </div>
+        <div class="brand-story">
             <h3>Brand Story Teaser</h3>
             <p>QuébecIpod was born from a passion of sound and simplicity</p>
-            <a href="about.php">About us</a>
+            <div class="buttons">
+                <a class="button-about-us" href="../about.php">About Us</a>
+            </div>
         </div>
+    </section>
+    <section class="info-home-additional">
+        <h3>Stay tuned for exclusive drops and retro revivals.</h3>
+        <form action="#" method="post">
+            <input type="email" class="home-form-input" placeholder="Your Email" required>
+            <button type="submit" class="home-form-button">Subscribe</button>
+        </form>
     </section>
 </main>
 <?php require "./templates/footer.php"; ?>
