41a02398912e28689f015692fd57258961f6fe33
Vlad <onixxwork@gmail.com>
2025-11-12 14:45:54 -0500

    adds the test version of the product page
    Did some layout with still static content, the page will be modified in the future

--- shop.php
+++ shop.php
@@ -9,27 +9,45 @@
             <h1>Discover Timeless Sound</h1>
             <p>Relieve the golden era from sound to the life</p>
         </div>
-        <div>
-            <a href="contact.php" class="button-shop">Contact Us</a>
-            <a href="about.php" class="button-about">Learn More</a>
-        </div>
     </section>
     <section class="product-section">
         <div>
-            <img src="./" alt="product1">
-            <h2>Product 1</h2>
+            <img src="../images/red.png" alt="red">
+            <h4>Product 1</h4>
             <p>20.50$</p>
         </div>
         <div>
-            <img src="./" alt="product2">
-            <h2>Product 2</h2>
+            <img src="../images/green1.png" alt="green1">
+            <h4>Product 2</h4>
             <p>20.47$</p>
         </div>
         <div>
-            <img src="./" alt="product2">
-            <h2>Product 3</h2>
+            <img src="../images/green2.png" alt="green2">
+            <h4>Product 3</h4>
             <p>20.45$</p>
         </div>
+        <div>
+            <img src="../images/blue.png" alt="blue">
+            <h4>Product 4</h4>
+            <p>20.45$</p>
+        </div>
+        <div>
+            <img src="../images/grey.png" alt="grey">
+            <h4>Product 5</h4>
+            <p>20.45$</p>
+        </div>
+        <div>
+            <img src="../images/purple.png" alt="purple">
+            <h4>Product 6</h4>
+            <p>20.45$</p>
+        </div>
+    </section>
+    <section class="info-additional">
+        <h3>Stay tuned for exclusive drops and retro revivals.</h3>
+        <form action="#" method="post">
+            <input type="email" class="additional-form-input" placeholder="Your Email" required>
+            <button type="submit" class="additional-form-button">Subscribe</button>
+        </form>
     </section>
 </main>
 <?php require "./templates/footer.php"; ?>
